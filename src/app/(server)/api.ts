3a163bc353c37bc56ebf0e1f9d4b6ba06982cbcb
Va7ul <vas7ul@gmail.com>
2025-02-18 03:42:51 +0200

    Update api.ts

--- src/app/(server)/api.ts
+++ src/app/(server)/api.ts
@@ -51,7 +51,7 @@ export const refreshUser = async () => {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
+      // const uid = user.uid;
       // ...
     } else {
       // User is signed out
