cdbabe8efaa3ebc4178362921a9684d9b36ed5f4
Va7ul <vas7ul@gmail.com>
2025-02-18 19:46:58 +0200

    log in modal

--- src/app/components/SignInPage/SignIn.tsx
+++ src/app/components/SignInPage/SignIn.tsx
@@ -1,33 +1,78 @@
 'use client';
 
 import clsx from 'clsx';
-import styles from './SignIn.module.css';
-// import { signIn } from '@/app/(server)/api';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Image from 'next/image';
+import styles from './SignIn.module.css';
+import { signIn } from '@/app/(server)/api';
+import { schema } from '@/lib/schemes/login';
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export const SignIn = () => {
-  // const email = 'test@ukr.net';
-  // const password = '123456';
   const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      email: 'test@ukr.net',
+      password: '123456',
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const onShow = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const onSubmit: SubmitHandler<Inputs> = ({ email, password }) => {
+    signIn(email, password);
+    router.back();
+  };
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles['text-container']}>
+    <form className={styles.wrapper} onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.container}>
         <h3>Log In</h3>
         <p>
           Welcome back! Please enter your credentials to access your account and
           continue your search for a psychologist.
         </p>
       </div>
-      <button
-        className={clsx(styles.button, 'btn-secondary')}
-        onClick={() => {
-          // signIn(email, password);
-          router.back();
-        }}
-      >
+      <div>
+        <div className={styles.container}>
+          <input {...register('email')} placeholder="Email" />
+          <span>{errors.email?.message}</span>
+        </div>
+        <div className={clsx(styles.container, styles.password)}>
+          <input
+            {...register('password')}
+            placeholder="Password"
+            type={showPassword ? 'text' : 'password'}
+          />
+          <Image
+            src={showPassword ? '/icons/eye-off.svg' : '/icons/eye.svg'}
+            alt={showPassword ? 'Open eye icon' : 'Close eye icon'}
+            width={20}
+            height={20}
+            onClick={onShow}
+          />
+          <span>{errors.password?.message}</span>
+        </div>
+      </div>
+      <button type="submit" className={clsx(styles.button, 'btn-secondary')}>
         Log In
       </button>
-    </div>
+    </form>
   );
 };
